2b1ea62171cf7a7f872cfc40eac9785a83587e0c
max-student <max-student@maxtrain.com>
2020-03-06 14:27:13 -0500

    initial commit

---
+++ PrsCapstone/Data/PrsCapstoneContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrsCapstone.Models {
+    public class PrsCapstoneContext : DbContext {
+        public PrsCapstoneContext(DbContextOptions<PrsCapstoneContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder model) {
+            model.Entity<User>(e => {
+                e.ToTable("Users");
+                e.HasIndex(x => x.Username).IsUnique();
+            });
+        }
+    }
+}
